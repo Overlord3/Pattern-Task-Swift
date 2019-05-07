cb31844754aeedd4cb0efad9f6389102ea7b0324
Александр Плесовских <sasha26141@nextmail.ru>
2019-05-07 12:09:00 +0300

    Создал проект

---
+++ Pattern-Task-Swift/Pattern-Task-Swift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pattern-Task-Swift
+//
+//  Created by Александр Плесовских on 07/05/2019.
+//  Copyright © 2019 Alex. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
